c4f3948b3275ecd3149e4a70b66096247a1651e5
abhidivekar9730 <abhishekdivekar4829@gmail.com>
2025-06-28 08:44:23 +0530

    DSA

--- src/IntroTopics/ternaryoperater.java
+++ src/IntroTopics/ternaryoperater.java
@@ -1,9 +1,13 @@
 package IntroTopics;
 
+import java.util.Scanner;
+
 public class ternaryoperater {
     public static void main(String[] args) {
+        System.out.println("enter tour mark : ");
+        Scanner sc = new Scanner(System.in);
 
-        int mark = 75;
+        int mark = sc.nextInt();
 
         String result = (mark > 70) ? "pass": "fail";
 
